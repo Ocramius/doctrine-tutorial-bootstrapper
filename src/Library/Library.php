08610f890acae05bfae1110eb05908b9b217852e
Marco Pivetta <ocramius@gmail.com>
2016-04-28 15:19:47 +0200

    Stub implementation of a library "lend" functionality

--- src/Library/Library.php
+++ src/Library/Library.php
@@ -28,4 +28,11 @@ class Library
     {
         return $this->repository->getAmount($this->libraryId, $isbn)->toInt();
     }
+
+    public function lend(ISBN $isbn, UserId $userId, \DateTimeImmutable $lendStartTime)
+    {
+        // ... check if the user is allowed (quota)
+        // ... check if inventory has book (also compute other lent books)
+        // create "lent" track, log at which time the book was given to the user
+    }
 }
