c60e73a8a65dc8f459f32d29362389ed7beaf251
Marco Pivetta <ocramius@gmail.com>
2016-04-28 22:13:03 +0200

    Page that uses the command bus to add a book to the library

--- public/add-book.php
+++ public/add-book.php
@@ -1,4 +1,9 @@
 <?php
 
-// add a book (with a certain amount) to the inventory
-// bonus: only a librarian can do that
+$commandBus = require __DIR__ . '/../command-bus.php';
+
+$commandBus(new \Library\Command\AddBook(
+    \Library\LibraryId::fromString('b7c0d51c-218b-4724-b7f9-796d3b206f52'),
+    \Library\ISBN::fromString(str_repeat('1', 13)),
+    \Library\Amount::fromInteger(11)
+));
