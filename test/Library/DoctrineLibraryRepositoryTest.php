fb35b0ad6f8cf0013eed996b8b30e039bd62ea3c
Marco Pivetta <ocramius@gmail.com>
2016-04-28 12:15:12 +0200

    Basic specification for how a repository for book amounts should behave when no book amount exists

---
+++ test/Library/DoctrineLibraryRepositoryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace LibraryTest;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Library\BookAmount;
+use Library\DoctrineLibraryRepository;
+use Ramsey\Uuid\Uuid;
+
+final class DoctrineLibraryRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ObjectRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $doctrineRepository;
+
+    /**
+     * @var ObjectManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $objectManager;
+
+    /**
+     * @var DoctrineLibraryRepository
+     */
+    private $repository;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws \PHPUnit_Framework_Exception
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->doctrineRepository = $this->getMock(ObjectRepository::class);
+        $this->objectManager      = $this->getMock(ObjectManager::class);
+        $this->repository         = new DoctrineLibraryRepository($this->doctrineRepository, $this->objectManager);
+    }
+
+    public function testGetAmountWithNonExistingAmount()
+    {
+        $libraryId = (string) Uuid::uuid4();
+        $isbn      = mt_rand(1234567890000, 1234567890123);
+
+        self::assertEquals(new BookAmount($libraryId, $isbn, 0), $this->repository->getAmount($libraryId, $isbn));
+    }
+}
