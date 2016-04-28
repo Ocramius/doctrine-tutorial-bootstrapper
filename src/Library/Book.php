9a66de7d8705a16da2df5675c8af20ba7660e4f7
Marco Pivetta <ocramius@gmail.com>
2016-04-28 11:56:03 +0200

    Dummy book object - won't be used for now

---
+++ src/Library/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Library;
+
+// NOTE: ignore for now - this is just to show that a book description is external to our concept of
+// "library inventory", for now
+class Book
+{
+    public function getIsbn()
+    {
+        // ..
+    }
+
+    public function getAttributes()
+    {
+
+    }
+}
