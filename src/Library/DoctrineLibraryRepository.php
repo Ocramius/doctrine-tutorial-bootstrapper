69dc22ccc45b78dd3fb507f7c511acd85f0af635
Marco Pivetta <ocramius@gmail.com>
2016-04-28 12:30:58 +0200

    Implementation of a doctrine-specific book amount repository

---
+++ src/Library/DoctrineLibraryRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Library;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+
+final class DoctrineLibraryRepository implements LibraryRepository
+{
+    /**
+     * @var ObjectRepository
+     */
+    private $bookAmountRepository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    public function __construct(
+        ObjectRepository $bookAmountRepository,
+        ObjectManager $objectManager
+    ) {
+        $this->bookAmountRepository = $bookAmountRepository;
+        $this->objectManager        = $objectManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAmount($libraryId, $isbn)
+    {
+        return $this->getOrCreateAmount($libraryId, $isbn);
+    }
+
+    public function setAmount($libraryId, BookAmount $amount)
+    {
+        $this->objectManager->persist($amount);
+        $this->objectManager->flush();
+    }
+
+    private function getOrCreateAmount($libraryId, $isbn)
+    {
+        $bookAmount = $this->bookAmountRepository->findOneBy([
+            'libraryId' => $libraryId,
+            'isbn'      => $isbn,
+        ]);
+
+        return $bookAmount instanceof BookAmount ? $bookAmount : new BookAmount($libraryId, $isbn, 0);
+    }
+}
